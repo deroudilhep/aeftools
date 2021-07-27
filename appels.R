9802406b12119f85557e36ea01263a344104e93d
Pierre <pierre.deroudilhe@protonmail.com>
2021-07-27 10:49:30 +0200

    major changes
    added a document that source R scripts everytime + paris map to call when needed

---
+++ appels.R
@@ -0,0 +1,19 @@
+packages <- c("tidyverse", "readr", "readxl", "sf",
+              "cowplot", "RColorBrewer", "lubridate",
+              "data.table","datapasta","devtools","forcats",
+              "geofacet","gghighlight","janitor","hexbin",
+              "rmapshaper","scales","svglite","extrafont","slider")
+
+lapply(packages, library, character.only = TRUE)
+
+communes <- function() {
+  communes <- st_read("C:/Users/pierr/Documents/JOURNALISME/APPELS CARTES R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/COMMUNE.shp")
+}
+
+dep <- function() {
+  deps <- sf::st_read("C:/Users/pierr/Documents/JOURNALISME/APPELS CARTES R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/DEPARTEMENT.SHP")
+}
+
+paris <- function() {
+  paris <- st_read("https://raw.githubusercontent.com/deroudilhep/data/main/arrondissements-paris.geojson")
+}
