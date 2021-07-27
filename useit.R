d20e7baf63d1afe80a757fc2428fb08545d22e09
Pierre <pierre.deroudilhe@protonmail.com>
2021-07-27 12:20:46 +0200

    Update useit.R

--- useit.R
+++ useit.R
@@ -7,11 +7,11 @@ extrafont::loadfonts()
 lapply(packages, library, character.only = TRUE)
 
 communes <- function() {
-  communes <- sf::st_read("C:/Users/pierr/Documents/JOURNALISME/APPELS_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/COMMUNE.shp")
+  communes <- sf::st_read("~/JOURNALISME/APPEL_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/COMMUNE.shp")
 }
 
 deps <- function() {
-  deps <- sf::st_read("C:/Users/pierr/Documents/JOURNALISME/APPEL_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/DEPARTEMENT.shp")
+  deps <- sf::st_read("~/JOURNALISME/APPEL_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/DEPARTEMENT.shp")
 }
 
 paris <- function() {
