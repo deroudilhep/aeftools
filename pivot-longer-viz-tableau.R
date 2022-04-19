dbc39adc340eecab0f143e2bbc6487339796713c
Pierre <83703658+deroudilhep@users.noreply.github.com>
2022-04-19 16:25:57 +0200

    Update pivot-longer-viz-tableau.R

--- pivot-longer-viz-tableau.R
+++ pivot-longer-viz-tableau.R
@@ -1,21 +1,16 @@
 library(readr)
 library(tidyverse)
 
-data <- read_delim("viz-tableau4.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+data <- read_delim("data-viz-tableau-no-pivot.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE) %>%
+  select(-candidats_part, -admis_part)
 
 parts <- data %>%
-  mutate("Filles parmi les admis" = `Dont nombre d'admises`*100/Admis,
-         "Filles parmi les candidats" = `Dont nombre de candidates`*100/Candidats)
+  mutate(candidats_part = candidats_filles*100/candidats_tous,
+         admis_part = admis_filles*100/admis_tous)
 
 pivot <- parts %>%
-  rename("admis_tous" = Admis, 
-         "candidats_tous" = Candidats, 
-         "admis_filles" = `Dont nombre d'admises`, 
-         "candidats_filles" = `Dont nombre de candidates`,
-         "admis_part" = `Filles parmi les admis`,
-         "candidats_part" = `Filles parmi les candidats`) %>% 
   pivot_longer(cols = c(admis_tous, candidats_tous, admis_filles, candidats_filles, admis_part, candidats_part), 
                names_to = c("Type", ".value"), 
                names_pattern = "(.+)_(.+)")
 
-write_delim(rename(pivot, "Tous" = tous, "Filles" = filles, "% des filles" = part), delim = ";", "viz-tableau-5-delim.csv")
+write_delim(rename(pivot, "Tous" = tous, "Filles" = filles, "% des filles" = part), delim = ";", "viz-tableau1.csv")
