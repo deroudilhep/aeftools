50508ab5098450d502c7f8cd25d885401ae416ac
Pierre <pierre.deroudilhe@protonmail.com>
2023-02-13 17:52:12 +0100

    recreated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # deroudilhep/data
 
-Voici le "repo" dans lequel je range certains outils et bouts de code que j'utilise dans le cadre de mon travail de journaliste. N'hésitez pas à l'exploiter, et en cas de question [contactez-moi](mailto:pierre.deroudilhe@protonmail.com) !
+`data` est le "repo" GitHub dans lequel je range certains outils et bouts de code que j'utilise dans le cadre de mon travail de journaliste. Il est libre d'utilisation. En cas de question, [contactez-moi](mailto:pierre.deroudilhe@protonmail.com) !
 
-*This is the repository where I put some of the tools and code snippets I use as a journalist. Feel free to use it and to [contact me](mailto:pierre.deroudilhe@protonmail.com) if you have any question!*
+*`data` is the repository where I put some of the tools and code snippets I use as a journalist. It's free of use. If you have any question, feel free to [email me](mailto:pierre.deroudilhe@protonmail.com).
--- useit.R
+++ useit.R
@@ -1,37 +1,15 @@
-packages <- c("tidyverse", "readr", "readxl", "sf", "cowplot", "RColorBrewer", "lubridate", "data.table", "datapasta", "devtools", "forcats", "geofacet", "gghighlight", "janitor", "hexbin", "rmapshaper", "scales", "svglite", "extrafont", "slider", "units", "udunits2")
-
-extrafont::loadfonts()
-
-`%!in%` = function(x,y) !(x %in% y)
-
-lapply(packages, library, character.only = TRUE)
-
-communes <- function() {
-  communes <- sf::st_read("~/JOURNALISME/APPEL_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/COMMUNE.shp")
+packages <- c("tidyverse", "readr", "readxl", "lubridate", "devtools")
+make_names <- function(names) {
+  names <- names %>%
+    str_trim() %>%
+    tolower() %>%
+    str_replace_all(" ", "_") %>%
+    str_replace_all("[àâä]", "a") %>%
+    str_replace_all("[éèêë]", "e") %>%
+    str_replace_all("[ïî]", "i") %>%
+    str_replace_all("[ôö]", "o") %>%
+    str_replace_all("[ùûü]", "u") %>%
+    str_replace_all("[ÿ]", "y") %>%
+    str_replace_all("[ç]", "c")
+  return(names)
 }
-
-deps <- function() {
-  deps <- sf::st_read("~/JOURNALISME/APPEL_CARTES_R/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2021-03-29/ADE_2-5_SHP_LAMB93_FR/DEPARTEMENT.shp")
-}
-
-paris <- function() {
-  paris <- sf::st_read("https://raw.githubusercontent.com/deroudilhep/data/main/arrondissements-paris.geojson")
-}
-
-jpegPlease <- function(x,y,z,titre) {
-  jpeg(filename = paste0(titre,".jpg"), width = y, height = z, quality = 100, units  = "px",type = "cairo")
-  plot(x)
-  dev.off()
-}
-
-svgPlease <- function(x,y,z,titre) {
-  svglite::svglite(file = paste0(titre,".svg"),width = y,height = z,bg = "white",standalone = T)
-  plot(x)
-  dev.off()
-}
-
-framboiseGradiant <- c("#FFDFD6", "#FDCCC7", "#FFA7A3", "#FE8E8D", "#F76566", "#E75059", "#E1414D", "#D12030", "#D0222C", "#C00E28", "#8B131C", "#591E1A", "#461916", "#3A1816", "#1B1B1B")
-
-myrtilleGradiant <- c("#E1E2E6", "#D7DADF", "#CAD0DC", "#BFC7DA", "#A7B8D6", "#8CA8D0", "#6F96CF", "#6388CF", "#3E6DC9", "#0355C5", "#0243BB", "#033EBC", "#003E9D", "#153D78", "#1C1C1C")
-
-roseGradiant <- c("#FAD3E6","#F19FC5","#EC6083","#E60146","#8A1003","#4E0800")
