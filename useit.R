19e458f2692ebbedce26b3b2a726c0f8bb2b1077
Pierre <pierre.deroudilhe@protonmail.com>
2023-03-01 12:30:16 +0100

    Update useit.R

--- useit.R
+++ useit.R
@@ -1,2 +1,2 @@
-packages <- c("tidyverse", "readr", "readxl", "lubridate", "devtools", "janitor", "jsonlite", "svglite", "hrbrthemes", "extrafonts")
+packages <- c("tidyverse", "readr", "readxl", "lubridate", "devtools", "janitor", "jsonlite", "svglite", "hrbrthemes", "extrafont")
 lapply(packages, library, character.only = T)
