5a03b24f0cf36b53a730c846ef1cb61bea2d1655
Pierre <pierre.deroudilhe@protonmail.com>
2023-03-01 10:37:47 +0100

    Update useit.R

--- useit.R
+++ useit.R
@@ -1,16 +1,2 @@
-packages <- c("tidyverse", "readr", "readxl", "lubridate", "devtools", "janitor")
+packages <- c("tidyverse", "readr", "readxl", "lubridate", "devtools", "janitor", "jsonlite", "svglite")
 lapply(packages, library, character.only = T)
-make_names <- function(names) {
-  names <- names %>%
-    str_trim() %>%
-    tolower() %>%
-    str_replace_all(" ", "_") %>%
-    str_replace_all("[àâä]", "a") %>%
-    str_replace_all("[éèêë]", "e") %>%
-    str_replace_all("[ïî]", "i") %>%
-    str_replace_all("[ôö]", "o") %>%
-    str_replace_all("[ùûü]", "u") %>%
-    str_replace_all("[ÿ]", "y") %>%
-    str_replace_all("[ç]", "c")
-  return(names)
-}
