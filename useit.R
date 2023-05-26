0cd3b01393163b0af870455850631b50ec759bb6
Pierre <pierre.deroudilhe@protonmail.com>
2023-05-26 14:54:18 +0200

    hiho

--- useit.R
+++ useit.R
@@ -1,3 +1,4 @@
 # Packages
 if(!require(pacman)) install.packages("pacman")
+library(pacman)
 pacman::p_load("tidyverse", "rvest", "RSelenium", "wdman", "netstat", "DescTools", "janitor", "extrafont", "knitr", "jsonlite", "devtools", "roxygen2", "testthat", "aefplot")
