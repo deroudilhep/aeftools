9aba9ca024db4f73b95e8cd0e3765792b11d041a
Pierre Deroudilhe <pierre.deroudilhe@aefinfo.fr>
2023-09-21 15:14:00 +0200

    Delete useit.R

--- useit.R
+++
@@ -1,4 +0,0 @@
-# Packages
-if(!require(pacman)) install.packages("pacman")
-library(pacman)
-pacman::p_load("tidyverse", "rvest", "RSelenium", "wdman", "netstat", "DescTools", "janitor", "extrafont", "knitr", "jsonlite", "devtools", "roxygen2", "testthat", "aefplot")
